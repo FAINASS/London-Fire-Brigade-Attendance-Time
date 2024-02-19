3907139a3d71fb13e22be12d714c0ecc759bb8c1
FAINASS <160468691+FAINASS@users.noreply.github.com>
2024-02-19 14:09:46 +0100

    Update 1_Enjeux_du_projet.py

--- pages/1_Enjeux_du_projet.py
+++ pages/1_Enjeux_du_projet.py
@@ -52,7 +52,9 @@ def main():
 
     <br>
     Le temps de réponse des casernes lors d’une urgence est un facteur majeur pour la limitation des dégâts à la fois physique et matériel. 
-    
+
+    <br>
+    <br>
     #### Objectifs 
     
     La LFB s’est fixée deux objectifs majeurs :
