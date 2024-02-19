da086238d7f82f00e77fcf053093f0567f83d13f
FAINASS <160468691+FAINASS@users.noreply.github.com>
2024-02-19 12:09:50 +0100

    Update 4_Modélisation.py

--- pages/4_Modélisation.py
+++ pages/4_Modélisation.py
@@ -173,7 +173,7 @@ def main():
        colsample_bytree = my_expander2.slider('Colsample bytree', min_value=0.1, max_value=1.0, value=0.7746831999163204)
        learning_rate = my_expander2.slider('Learning rate', min_value=0.1, max_value=1.0, value=0.0624207548570334)
        max_depth = my_expander2.slider('Max Depth', min_value=1, max_value=12, value=6)
-       min_child_weight = my_expander2.slider('Min child weight', min_value=1, max_value=5, value=1)
+       min_child_weight = my_expander2.slider('Min child weight', min_value=1, max_value=5, value=3)
        n_estimators = my_expander2.slider('N_estimators', min_value=100, max_value=1200, value=441)
 
        model = XGBRegressor(colsample_bytree =colsample_bytree,  
