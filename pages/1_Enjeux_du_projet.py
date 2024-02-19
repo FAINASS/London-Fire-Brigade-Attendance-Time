158446e2994c552e439e97662494306c4fd0e813
FAINASS <160468691+FAINASS@users.noreply.github.com>
2024-02-19 14:06:12 +0100

    Update 1_Enjeux_du_projet.py

--- pages/1_Enjeux_du_projet.py
+++ pages/1_Enjeux_du_projet.py
@@ -51,7 +51,7 @@ def main():
     | Nombre de casernes de pompiers | <span style='color:lightblue'>103</span> |
 
     
-    Le temps de réponse des casernes lors d’une urgence est un facteur majeur pour la limitation des dégâts à la fois physique et matériel. 
+    \nLe temps de réponse des casernes lors d’une urgence est un facteur majeur pour la limitation des dégâts à la fois physique et matériel. 
     
     #### Objectifs 
     
