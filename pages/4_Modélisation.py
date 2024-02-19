2a52a76423f783dd7982a56dcc216c731bfba310
FAINASS <160468691+FAINASS@users.noreply.github.com>
2024-02-19 19:59:42 +0100

    Update 4_Modélisation.py

--- pages/4_Modélisation.py
+++ pages/4_Modélisation.py
@@ -177,15 +177,14 @@ def main():
     if model_type == 'Ridge':
         alpha = my_expander2.slider('Alpha', min_value=1.0, max_value=50.0, value=9.372353071731432)
         model = Ridge(alpha=alpha)
-    
-    
-   else :
+        
+    else:
         colsample_bytree = my_expander2.slider('Colsample bytree', min_value=0.1, max_value=1.0, value=0.9194586567111567)
         learning_rate = my_expander2.slider('Learning rate', min_value=0.1, max_value=1.0, value=0.19789386803938744)
         max_depth = my_expander2.slider('Max Depth', min_value=1, max_value=12, value=3)
         min_child_weight = my_expander2.slider('Min child weight', min_value=1, max_value=5, value=2)
         n_estimators = my_expander2.slider('N_estimators', min_value=100, max_value=1200, value=441)
-        
+    
         model = LGBMRegressor(colsample_bytree=colsample_bytree,
                               learning_rate=learning_rate,
                               max_depth=max_depth,
