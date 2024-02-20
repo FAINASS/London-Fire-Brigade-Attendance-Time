eeafaff32decfb188c6d2c12b907fb86118bff88
FAINASS <160468691+FAINASS@users.noreply.github.com>
2024-02-20 21:24:40 +0100

    Update 2_Collecte_des_données.py

--- pages/2_Collecte_des_données.py
+++ pages/2_Collecte_des_données.py
@@ -37,6 +37,17 @@ def add_logo():
         unsafe_allow_html=True,
     )
 
+st.markdown("""
+        <style>
+               .block-container {
+                    padding-top: 1rem;
+                    padding-bottom: 0rem;
+                    padding-left: 5rem;
+                    padding-right: 5rem;
+                }
+        </style>
+        """, unsafe_allow_html=True)
+
 def main():
     
     add_logo()
