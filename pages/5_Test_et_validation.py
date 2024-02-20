7018e324e7a968f0449f2d25c2581343cc55d2ca
FAINASS <160468691+FAINASS@users.noreply.github.com>
2024-02-20 11:18:58 +0100

    Update 5_Test_et_validation.py

--- pages/5_Test_et_validation.py
+++ pages/5_Test_et_validation.py
@@ -112,11 +112,11 @@ def main():
     selected_boroughs = col3.selectbox("Arrondissement:", boroughs, index=boroughs.index(st.session_state['incident']['BoroughName']),disabled=True)
     df_filtreBoroughs = df[df['BoroughName'] == selected_boroughs]
     
-    wards = sorted(df_filtreBoroughs['WardName'].unique().tolist())
+    wards = sorted(df['WardName'].unique().tolist())
     selected_wards= col4.selectbox("Quartier:", wards, index=wards.index(st.session_state['incident']['WardName']))
-    df_filtreWards = df_filtreBoroughs[df_filtreBoroughs['WardName'] == selected_wards]
+    df_filtreWards = df_filtreBoroughs[df['WardName'] == selected_wards]
     
-    station = sorted(df_filtreWards['DeployedFromStationName'].unique().tolist())
+    station = sorted(df['DeployedFromStationName'].unique().tolist())
     selected_station= col5.selectbox("Première caserne déployée:", station, index=station.index(st.session_state['incident']['DeployedFromStationName']))
     
  
