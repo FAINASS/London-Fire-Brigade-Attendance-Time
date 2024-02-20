14dad37af57f0b1e80e37c7e38a025468cff987d
FAINASS <160468691+FAINASS@users.noreply.github.com>
2024-02-20 12:03:10 +0100

    Update 5_Test_et_validation.py

--- pages/5_Test_et_validation.py
+++ pages/5_Test_et_validation.py
@@ -224,7 +224,7 @@ def main():
             time.sleep(0.05)
 
         difference = prediction - st.session_state['incident']['AttendanceTime'] 
-        st.markdown(f"<h3 style='text-align: center;'> Nous avons une erreur de prédiction de : {difference:.2f} min</h3>", unsafe_allow_html=True)
+        st.markdown(f"<h3 style='text-align: center; font-size: 8px;'> Nous avons une erreur de prédiction de : {difference:.2f} min</h3>", unsafe_allow_html=True)
     
     except UnboundLocalError:
         st.write('')
