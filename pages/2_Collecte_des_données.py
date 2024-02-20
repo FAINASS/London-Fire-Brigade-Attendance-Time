ab95e1343c6afbad79ecbc1b6bb512d4132ad517
FAINASS <160468691+FAINASS@users.noreply.github.com>
2024-02-20 21:56:41 +0100

    Update 2_Collecte_des_données.py

--- pages/2_Collecte_des_données.py
+++ pages/2_Collecte_des_données.py
@@ -42,7 +42,7 @@ st.markdown("""
         <style>
                .block-container {
                     padding-top: 1rem;
-                    padding-bottom: 1rem;
+                    padding-bottom: 2rem;
                     padding-left: 5rem;
                     padding-right: 5rem;
                 }
