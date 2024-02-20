cf05d91e18a06e825b9ffc9fcf6b93160ccd4451
FAINASS <160468691+FAINASS@users.noreply.github.com>
2024-02-20 12:19:45 +0100

    Update 5_Test_et_validation.py

--- pages/5_Test_et_validation.py
+++ pages/5_Test_et_validation.py
@@ -226,6 +226,7 @@ def main():
         difference = prediction - st.session_state['incident']['AttendanceTime'] 
         secondes = difference * 60
         minutes, secondes = divmod(secondes, 60)
+        st.write(difference)
         st.markdown(f"<h3 style='text-align: center; font-size: 20px;'><i>Nous avons une erreur de prédiction de : {minutes:.0f} minute(s) et {secondes:.0f} seconde(s)</i></h3>", unsafe_allow_html=True)
     
     except UnboundLocalError:
