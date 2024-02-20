951753db3d1e0c3b5634bbf32b8e082666cc5627
FAINASS <160468691+FAINASS@users.noreply.github.com>
2024-02-20 22:27:17 +0100

    Update 5_Test_et_validation.py

--- pages/5_Test_et_validation.py
+++ pages/5_Test_et_validation.py
@@ -48,7 +48,7 @@ st.markdown("""
         <style>
                .block-container {
                     padding-top: 1rem;
-                    padding-bottom: 4rem;
+                    padding-bottom: 5rem;
                     padding-left: 5rem;
                     padding-right: 5rem;
                 }
