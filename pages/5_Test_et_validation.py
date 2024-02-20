5d34c69726f6337a4b3490295b840e973e6f17ec
FAINASS <160468691+FAINASS@users.noreply.github.com>
2024-02-20 12:30:46 +0100

    Update 5_Test_et_validation.py

--- pages/5_Test_et_validation.py
+++ pages/5_Test_et_validation.py
@@ -215,15 +215,14 @@ def main():
         st.header(" ")
         prediction = model.predict(X)[0]
         
-        # Créer un emplacement vide
+        secondes_total = abs(prediction) * 60
+        
         counter = st.empty()
-        # Boucle pour mettre à jour le compteur
-        for i in np.arange(0, prediction, 0.1): 
-            secondes = abs(prediction)
-            minutes, secondes = divmod(i * 60)
-            counter.markdown(f"<h3 style='text-align: center; color: White;'>Le temps de réponse estimé est {prediction:.2f} soit : <span style='color: Orange;'>{int(minutes)}</span> minute(s) et <span style='color: Orange;'>{int(secondes)}</span> seconde(s).</h3>", unsafe_allow_html=True)
+        for i in np.arange(0, secondes_total, 0.1): 
+            minutes, secondes = divmod(i, 60)
+            counter.markdown(f"<h3 style='text-align: center; color: White;'>Le temps de réponse estimé est {secondes_total:.2f} secondes soit : <span style='color: Orange;'>{int(minutes)}</span> minute(s) et <span style='color: Orange;'>{int(secondes)}</span> seconde(s).</h3>", unsafe_allow_html=True)
             time.sleep(0.05)
-
+            
         difference = prediction - st.session_state['incident']['AttendanceTime'] 
         secondes = abs(difference) * 60
         minutes, secondes = divmod(secondes, 60)
