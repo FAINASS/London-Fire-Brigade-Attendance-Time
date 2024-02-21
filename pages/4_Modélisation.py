ce27acda8dfa72cd887a9c5cb759b85c6ca8c4c5
FAINASS <160468691+FAINASS@users.noreply.github.com>
2024-02-21 15:31:04 +0100

    Update 4_Modélisation.py

--- pages/4_Modélisation.py
+++ pages/4_Modélisation.py
@@ -201,10 +201,11 @@ def main():
                 
                 ##### Type d'encodeur : 
                 - Ordinal Encoder : 
-                    - C’est comme donner un numéro à chaque élément d’une liste. Par exemple, dans une liste de fruits, on pourrait dire que la pomme est le numéro 1, la banane le numéro 2, et ainsi de suite.
+                    - C’est donner un numéro à chaque élément d’une liste. Par exemple, dans une liste de fruits, on pourrait dire que la Pomme est le numéro 1, la Banane le numéro 2.
                     
                 - One-Hot Encoder :
-                    - pour chaque fruit, on crée une colonne. Si le fruit est présent, il met un 1 dans cette colonne. S'il n’est pas présent, il met un 0. Par exemple, si vous avez "pomme", on aurait : Pomme (1) Banane (0) soit 10.
+                    - Avec ce type d'encodeur, pour chaque fruit, on crée une colonne. Si le fruit est présent, il met un 1 dans cette colonne. S'il n’est pas présent, il met un 0. 
+                    - Par exemple, si vous avez "pomme", on aurait : Pomme (1) Banane (0) soit 10.
               """)
     
 ########################################################################################################################################################################################################################## 
