ab043ab6ab9cce8eb4cffd9ceba6fc48c9572546
FAINASS <160468691+FAINASS@users.noreply.github.com>
2024-02-22 11:17:29 +0100

    Update 5_Test_et_validation.py

--- pages/5_Test_et_validation.py
+++ pages/5_Test_et_validation.py
@@ -96,22 +96,25 @@ def main():
     # Vérifier si 'incident' est déjà dans session_state
     if 'incident' not in st.session_state:
         st.session_state['incident'] = selected_columns.iloc[88530]
-
+        
+    col1, col2 = st.columns(2) 
     IncidentGroupType = sorted(df['IncidentGroupType'].unique().tolist())
     selected_incidents = col1.selectbox("Catégorie d'incident:", IncidentGroupType, index=IncidentGroupType.index(0),disabled=True)
     
     propertyType = sorted(df['PropertyType'].unique().tolist())
     selected_property = col2.selectbox("Type d'emplacement:", propertyType, index=propertyType.index(0))
-    
+
+    col3, col4,col5 = st.columns(3) 
     boroughs = sorted(df['BoroughName'].unique().tolist())
     selected_boroughs = col3.selectbox("Arrondissement:", boroughs, index=boroughs.index(0),disabled=True)
-    
+
     wards = sorted(df_filtreBoroughs['WardName'].unique().tolist())
     selected_wards= col4.selectbox("Quartier:", wards, index=wards.index(0))
     
     station = sorted(df_filtreBoroughs['DeployedFromStationName'].unique().tolist())
     selected_station= col5.selectbox("Première caserne déployée:", station, index=station.index(0))
 
+    col6,col7,col8 = st.columns(3)
     Hour = list(np.arange(0.0, 24.0, 1.0))
     selectedHour = col6.selectbox("Heure de l'appel:", Hour, index=Hour.index(0))
      
@@ -145,7 +148,7 @@ def main():
     st.markdown("---")
     
     st.subheader("1. Type d'incident")
-    col1, col2 = st.columns(2) 
+    #col1, col2 = st.columns(2) 
     
     IncidentGroupType = sorted(df['IncidentGroupType'].unique().tolist())
     selected_incidents = col1.selectbox("Catégorie d'incident:", IncidentGroupType, index=IncidentGroupType.index(st.session_state['incident']['IncidentGroupType']),disabled=True)
@@ -157,7 +160,7 @@ def main():
     
     st.subheader(" ")
     st.subheader("2. Géolocalisation")
-    col3, col4,col5 = st.columns(3) 
+    #col3, col4,col5 = st.columns(3) 
     
     boroughs = sorted(df['BoroughName'].unique().tolist())
     selected_boroughs = col3.selectbox("Arrondissement:", boroughs, index=boroughs.index(st.session_state['incident']['BoroughName']),disabled=True)
@@ -202,7 +205,7 @@ def main():
     st.write(" ")
     st.subheader("3. Intervention")
     
-    col6,col7,col8 = st.columns(3)
+    #col6,col7,col8 = st.columns(3)
     
     Hour = list(np.arange(0.0, 24.0, 1.0))
     selectedHour = col6.selectbox("Heure de l'appel:", Hour, index=Hour.index(st.session_state['incident']['HourOfCall']))
