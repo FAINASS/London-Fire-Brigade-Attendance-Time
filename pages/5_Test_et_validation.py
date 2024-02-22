2ec0de0f52c934c8ab7c07ef543f3be466427a60
FAINASS <160468691+FAINASS@users.noreply.github.com>
2024-02-22 15:26:23 +0100

    Update 5_Test_et_validation.py

--- pages/5_Test_et_validation.py
+++ pages/5_Test_et_validation.py
@@ -240,15 +240,14 @@ def main():
             except UnboundLocalError:
                 st.write("")
 ##############################################################################################################################################################################################################################################################################################
-    import streamlit as st2
     
     with onglet2:
         df2 = load_data("df_Predictions.csv")
         st2.subheader("1. Type d'incident")
-        col9, col10 = st2.columns(2)
+        col9, col10 = st.columns(2)
         
         IncidentGroupType2 = sorted(df2['IncidentGroupType'].unique().tolist())
-        selected_incidents2 = col1.selectbox("Catégorie d'incident:", IncidentGroupType2)
+        selected_incidents2 = col9.selectbox("Catégorie d'incident:", IncidentGroupType2)
         
         
         st.subheader(" ")
