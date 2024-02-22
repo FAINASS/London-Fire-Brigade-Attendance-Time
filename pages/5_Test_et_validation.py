f24b335cd7c7cfb0e6e93e326fc167654edc325a
FAINASS <160468691+FAINASS@users.noreply.github.com>
2024-02-22 12:34:01 +0100

    Update 5_Test_et_validation.py

--- pages/5_Test_et_validation.py
+++ pages/5_Test_et_validation.py
@@ -225,12 +225,12 @@ def main():
             (st.session_state['incident']['DeployedFromStationName'] == selected_station) and \
             (st.session_state['incident']['NumStationsWithPumpsAttending'] == selected_NumPump) and \
             (st.session_state['incident']['SecondPumpArrivingDeployedFromStation'] == selected_secondPump) : 
-                    difference = prediction - st.session_state['incident']['AttendanceTime'] 
-                    secondes = abs(difference) * 60
-                    minutes, secondes = divmod(secondes, 60)
-                    st.markdown(f"<h3 style='text-align: center; font-size: 20px;'><i>Nous avons une erreur de prédiction de : {minutes:.0f} minute(s) et {secondes:.0f} seconde(s)</i></h3>", unsafe_allow_html=True)
-                else:
-                    st.write(" ")
+            difference = prediction - st.session_state['incident']['AttendanceTime'] 
+            secondes = abs(difference) * 60
+            minutes, secondes = divmod(secondes, 60)
+            st.markdown(f"<h3 style='text-align: center; font-size: 20px;'><i>Nous avons une erreur de prédiction de : {minutes:.0f} minute(s) et {secondes:.0f} seconde(s)</i></h3>", unsafe_allow_html=True)
+        else:
+            st.write(" ")
                 
     except UnboundLocalError:
         st.write("")
