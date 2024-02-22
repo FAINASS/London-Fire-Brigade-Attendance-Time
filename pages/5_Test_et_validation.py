b28616b44aae336bd881375ecc5a934f903692b5
FAINASS <160468691+FAINASS@users.noreply.github.com>
2024-02-22 16:09:48 +0100

    Update 5_Test_et_validation.py

--- pages/5_Test_et_validation.py
+++ pages/5_Test_et_validation.py
@@ -346,9 +346,8 @@ def main():
                     
             
             try:
-                
                 st.write(" ")
-                prediction2 = model.predict(X)[0]
+                prediction2 = model.predict(X2)[0]
                 secondes2 = abs(prediction2) * 60
                 minutes2, secondes2 = divmod(secondes, 60)
                 st.markdown(f"<h3 style='text-align: center; color: White;'>Le temps de réponse estimé est {prediction2:.2f} soit : <span style='color: Orange;'>{int(minutes2)}</span> minute(s) et <span style='color: Orange;'>{int(secondes2)}</span> seconde(s).</h3>", unsafe_allow_html=True)
