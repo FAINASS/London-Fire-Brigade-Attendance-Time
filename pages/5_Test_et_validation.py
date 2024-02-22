f52e8ecc8a6ae78aecbb201aba5588bd159f3564
FAINASS <160468691+FAINASS@users.noreply.github.com>
2024-02-22 01:57:54 +0100

    Update 5_Test_et_validation.py

--- pages/5_Test_et_validation.py
+++ pages/5_Test_et_validation.py
@@ -222,8 +222,6 @@ def main():
         minutes, secondes = divmod(secondes, 60)
         st.markdown(f"<h3 style='text-align: center; font-size: 20px;'><i>Nous avons une erreur de prédiction de : {minutes:.0f} minute(s) et {secondes:.0f} seconde(s)</i></h3>", unsafe_allow_html=True)
 
-        if selected_NumPump == 1 :
-            selected_secondPump = "No Second pump deployed"
             
     except UnboundLocalError:
         st.write('')
