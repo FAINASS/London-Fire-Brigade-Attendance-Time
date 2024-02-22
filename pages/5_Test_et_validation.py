cf5dac1a0a2c06e15556ec43ff2119d2253c1cfc
FAINASS <160468691+FAINASS@users.noreply.github.com>
2024-02-22 02:03:03 +0100

    Update 5_Test_et_validation.py

--- pages/5_Test_et_validation.py
+++ pages/5_Test_et_validation.py
@@ -217,11 +217,11 @@ def main():
         st.markdown(f"<h3 style='text-align: center; color: White;'>Le temps de réponse estimé est {prediction:.2f} soit : <span style='color: Orange;'>{int(minutes)}</span> minute(s) et <span style='color: Orange;'>{int(secondes)}</span> seconde(s).</h3>", unsafe_allow_html=True)
 
         if (st.session_state['incident']['NumStationsWithPumpsAttending'] == selected_NumPump) and 
-        (st.session_state['incident']['SecondPumpArrivingDeployedFromStation'] == selected_secondPump) : 
-            difference = prediction - st.session_state['incident']['AttendanceTime'] 
-            secondes = abs(difference) * 60
-            minutes, secondes = divmod(secondes, 60)
-            st.markdown(f"<h3 style='text-align: center; font-size: 20px;'><i>Nous avons une erreur de prédiction de : {minutes:.0f} minute(s) et {secondes:.0f} seconde(s)</i></h3>", unsafe_allow_html=True)
+   (st.session_state['incident']['SecondPumpArrivingDeployedFromStation'] == selected_secondPump):
+    difference = prediction - st.session_state['incident']['AttendanceTime'] 
+    secondes = abs(difference) * 60
+    minutes, secondes = divmod(secondes, 60)
+    st.markdown(f"<h3 style='text-align: center; font-size: 20px;'><i>Nous avons une erreur de prédiction de : {minutes:.0f} minute(s) et {secondes:.0f} seconde(s)</i></h3>", unsafe_allow_html=True)
 
             
     except UnboundLocalError:
