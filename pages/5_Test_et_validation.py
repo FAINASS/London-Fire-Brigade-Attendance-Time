cb6441fe788b54c3eff76c50fdfa20fa8a13e0ae
FAINASS <160468691+FAINASS@users.noreply.github.com>
2024-02-22 16:49:27 +0100

    Update 5_Test_et_validation.py

--- pages/5_Test_et_validation.py
+++ pages/5_Test_et_validation.py
@@ -316,7 +316,7 @@ def main():
 
         secondPump2 = sorted(df['DeployedFromStationName'].unique().tolist())
         secondPump2.append("No Second pump deloyed")
-        selected_secondPump2 = col8.selectbox("2ième caserne déployée:", secondPump2,index=secondPump2.index("No Second pump deployed"))
+        selected_secondPump2 = col8.selectbox("2ième caserne déployée:", secondPump2)
             
         st.subheader (" ")
     
