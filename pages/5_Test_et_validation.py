23a60e3479122a0c75ff88616afe6d9f10b0658e
FAINASS <160468691+FAINASS@users.noreply.github.com>
2024-02-22 13:34:58 +0100

    Update 5_Test_et_validation.py

--- pages/5_Test_et_validation.py
+++ pages/5_Test_et_validation.py
@@ -84,7 +84,7 @@ def main():
     st.write(" ")
 
     df = load_data("df_Predictions.csv")
-    df = df.drop(df[(df['NumStationsWithPumpsAttending'] > 1) & (df['SecondPumpArrivingDeployedFromStation'] != "No Second pump deployed")].index)
+    df = df.drop(df[(df['NumStationsWithPumpsAttending'] > 1) & (df['SecondPumpArrivingDeployedFromStation'] != "No Second pump deployed")])
     
     st.subheader("0. Incident à prédire")
     selected_columns = df[["IncidentGroupType", "PropertyType", "BoroughName", "WardName", "DeployedFromStationName","Distance",
